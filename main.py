eeb418c92802b44b4c5ae055bbcb9dc703b7e503
Paipai <qgatler@outlook.com>
2016-12-30 15:17:04 +0800

    第十一次提交

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import render_template
 app = Flask(__name__)
 # 设置 secret_key 来使用 flask 自带的 session
 # 这个字符串随便你设置什么内容都可以
-app.secret_key = 'fsdfhloip.ljhklh'
+app.secret_key = 'fsdfhl oip.ljhklh'
 # 这一行是套路
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
@@ -12,15 +12,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # 引入、注册蓝图
 # 有一个 url_prefix 可以用来给蓝图中的每个路由加一个前缀
 
-from weibo import main as weibo_routes
-from user import main as user_routes
-from blog import main as blog_routes
-from api import main as api_routes
+from message import main as message_route
+from user import main as user_route
+from bbs import main as bbs_route
+from api import main as api_route
 
-app.register_blueprint(weibo_routes)
-app.register_blueprint(user_routes)
-app.register_blueprint(blog_routes, url_prefix='/blog')
-app.register_blueprint(api_routes,
+app.register_blueprint(message_route)
+app.register_blueprint(user_route)
+app.register_blueprint(bbs_route, url_prefix='/bbs')
+app.register_blueprint(api_route,
                        url_prefix='/api')
 
 
--- message.py
+++ message.py
@@ -5,8 +5,8 @@ from flask import url_for
 from flask import Blueprint
 from flask import abort
 
-from models import Blog
-from models import BlogComment
+from models import Weibo
+from models import Comment
 
 from user import current_user
 
@@ -14,31 +14,33 @@ from user import current_user
 # 创建一个 蓝图对象 并且路由定义在蓝图对象中
 # 然后在 flask 主代码中「注册蓝图」来使用
 # 第一个参数是蓝图的名字，第二个参数是套路
-main = Blueprint('blog', __name__)
+main = Blueprint('message', __name__)
 
 
-@main.route('/')
+@main.route('/message')
 def index():
     u = current_user()
     if u is None:
         return redirect(url_for('user.login_view'))
-    blogs = Blog.query.order_by(Blog.id.desc()).all()
-    for i in blogs:
+    # 查找所有的 todo 并返回
+    weibo_list = Weibo.query.order_by(Weibo.id.desc()).all()
+    for i in weibo_list:
         i.comment = i.comments()
         for j in i.comment:
             j.avatar = j.get_avatar()
         i.comments_num = len(i.comment)
-    return render_template('blog_index.html', blogs=blogs)
+        i.avatar = i.get_avatar()
+    return render_template('message.html', messages=weibo_list)
 
 
 @main.route('/add', methods=['POST'])
 def add():
     form = request.form
     u = current_user()
-    b = Blog(form)
-    b.name = '3000'
-    if b.valid():
-        b.save()
+    t = Weibo(form)
+    t.name = u.username
+    if t.valid():
+        t.save()
     # 蓝图中的 url_for 需要加上蓝图的名字，这里是 todo
     return redirect(url_for('.index'))
 
@@ -47,27 +49,17 @@ def add():
 def comment():
     form = request.form
     u = current_user()
-    c = BlogComment(form)
+    c = Comment(form)
     c.name = u.username
     if c.valid():
         c.save()
-    c.avatar = c.get_avatar()
-    return c.json()
+    return redirect(url_for('.index'))
 
 
 @main.route('/delete/<int:weibo_id>')
 def delete(weibo_id):
-    """
-    <int:todo_id> 的方式可以匹配一个 int 类型
-    int 指定了它的类型，省略的话参数中的 todo_id 就是 str 类型
-
-    这个概念叫做 动态路由
-    意思是这个路由函数可以匹配一系列不同的路由
-
-    动态路由是现在流行的路由设计方案
-    """
     # 通过 id 查询 todo 并返回
-    w = Blog.query.get(weibo_id)
+    w = Weibo.query.get(weibo_id)
     # 删除
     w.delete()
     # 引用蓝图内部的路由函数的时候，可以省略名字只用 .
