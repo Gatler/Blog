adcd0bd32338cf42c9e8742138572a30bd33c5f6
盖伦 <815810335@qq.com>
2017-08-19 02:10:49 +0800

    简化

--- main.py
+++ main.py
@@ -2,15 +2,9 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-# 设置 secret_key 来使用 flask 自带的 session
-# 这个字符串随便你设置什么内容都可以
 app.secret_key = 'fsdfhl oip.ljhklh'
-# 这一行是套路
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-
-# 引入、注册蓝图
-# 有一个 url_prefix 可以用来给蓝图中的每个路由加一个前缀
 
 from message import main as message_route
 from user import main as user_route
@@ -34,7 +28,6 @@ def error404(e):
     return render_template('410.html')
 
 
-# 运行代码
 if __name__ == '__main__':
     config = dict(
         debug=True,
